c3eb071d0c6f1a1ff7dd9b1d44946a29bf10cb80
xzcxvc <daryl.tiamps@gmail.com>
2024-12-18 18:35:17 +0800

    frontend changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,25 @@ function App() {
   return (
     <div className="bg-[#36384D] w-screen min-h-screen flex items-center justify-center">
       <div className="container bg-white p-6 rounded-[40px] w-6/12 flex items-center justify-center">
-        <div className="w-7/12 px-8 py-16">
-          <h1 className="font-bold text-5xl">Stay updated!</h1>
+        <div className="w-7/12 ">
+          <div className="px-10 py-16 flex flex-col gap-4">
+            <h1 className="font-bold text-[64px]">Stay updated!</h1>
+            <p className="text-[20px]">
+              Join 60,000+ product managers receiving monthly updates on:
+            </p>
+            <ul className="text-[20px]">
+              <li>• Product discovery and building what matters</li>
+              <li>• Measuring to ensure updates are a success</li>
+              <li>• And much more!</li>
+            </ul>
+
+            <Button
+              name="buttonSubscribe"
+              label="Subscribe to monthly newsletter"
+            />
+          </div>
         </div>
         <div className="border-2 w-5/12">2</div>
-        {/* <Button
-          name="buttonSubscribe"
-          label="Subscribe to monthly newsletter"
-        /> */}
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,8 @@ const Button = ({ name, label, onClick }) => {
   return (
     <button
       name={name}
-      className={`w-full rounded-md bg-[#232742] text-white py-4 font-medium transition-all duration-100 hover:bg-gradient-to-br hover:from-[#FD5C6A] hover:to-[#FA6150] hover:shadow-xl hover:shadow-[#FA6150]`}
+      className={`button w-full rounded-md bg-[#232742] text-white py-4 font-medium transition-all duration-100 
+        hover:bg-gradient-to-br hover:from-[#FD5C6A] hover:to-[#FA6150]`}
     >
       {label}
     </button>
